b026068a986db89876023bce275fe624f2575a0e
Amalia <amalia_damayanti_23rpl@student.smktelkom-mlg.sch.id>
2016-12-07 13:22:58 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     public static final String HOTEL = "hotel";
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+    int itemPos;
     private int REQUEST_CODE_ADD;
     private int REQUEST_CODE_EDIT;
 
@@ -61,6 +62,11 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        } else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK) {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos, hotel);
             mAdapter.notifyDataSetChanged();
         }
     }
@@ -115,5 +121,28 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(HOTEL, mList.get(pos));
         startActivity(intent);
+    }
+
+    @Override
+    public void doEdit(int pos) {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int pos) {
+
+    }
+
+    @Override
+    public void doShare(int pos) {
+
     }
 }
